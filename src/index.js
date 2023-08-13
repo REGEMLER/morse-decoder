06c08de5b508e730e2d33fb48a6d0c6070360270
Yar Night <yaroslav239@gmail.com>
2023-08-13 20:42:48 +0300

    feat: task implement

--- src/index.js
+++ src/index.js
@@ -38,7 +38,45 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    const dot = '10';
+    const dash = '11';
+    let newString = expr.split('**********'); 
+    let decodedItem = '';
+    let codedItem = ''; 
+    let char = ''; 
+    let bin = ''; 
+    let decodedWord = '';
+    let result = '';
+
+    for (let i = 0; i < newString.length; i++) { 
+        for (let j = 0; j < newString[i].length; j+= 10) { 
+            bin = newString[i].slice([j], [j + 10]); 
+            
+            for (let k = 0; k < bin.length; k+= 2) { 
+                char = bin.slice([k], [k + 2]);
+                if (char === dot) { 
+                    codedItem += '.'
+                 }  else if (char === dash) { 
+                    codedItem += '-'
+                  } else { 
+                    codedItem = codedItem
+                   }
+            } 
+            
+            for (let key in MORSE_TABLE) {
+                if (key === codedItem) {
+                decodedItem = MORSE_TABLE[key];
+            } 
+        }  
+            decodedWord += decodedItem;
+            codedItem = '';
+        } 
+        result = result + decodedWord + ' ';
+        decodedWord = '';
+    } 
+    
+    return result.trim() 
 }
 
 module.exports = {
